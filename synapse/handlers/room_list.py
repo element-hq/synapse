efc6966a11ddcab469530b09a54379edb5a08671
Olivier Wilkinson (reivilibre) <oliverw@matrix.org>
2023-12-19 18:25:19 +0000

    fixup! Don't cache /publicRooms response for inbound federated requests

--- synapse/handlers/room_list.py
+++ synapse/handlers/room_list.py
@@ -103,7 +103,7 @@ class RoomListHandler:
             network_tuple,
         )
 
-        if search_filter and from_federation_origin is not None:
+        if search_filter or from_federation_origin is not None:
             # We explicitly don't bother caching searches or requests for
             # appservice specific lists.
             # We also don't bother caching requests from federated homeservers.
