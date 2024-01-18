aaf7ab1ea95300ab9dbba721f1c25804678779b4
Erik Johnston <erik@matrix.org>
2024-01-18 13:33:07 +0000

    Prepatory work for tweaking performance of auth chain lookups

--- synapse/storage/schema/__init__.py
+++ synapse/storage/schema/__init__.py
@@ -18,7 +18,7 @@
 #
 #
 
-SCHEMA_VERSION = 83  # remember to update the list below when updating
+SCHEMA_VERSION = 84  # remember to update the list below when updating
 """Represents the expectations made by the codebase about the database schema
 
 This should be incremented whenever the codebase changes its requirements on the
