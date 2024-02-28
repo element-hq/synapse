e1cc6846bbe358323752f02ad8e5be835c2b9c4a
Gerrit Gogel <gerrit@gogel.me>
2024-02-28 13:51:36 +0100

    use a list comprehension instead of set operation

--- synapse/event_auth.py
+++ synapse/event_auth.py
@@ -177,7 +177,7 @@ async def check_state_independent_auth_rules(
     # 2. Reject if event has auth_events that: ...
     if batched_auth_events:
         auth_events = batched_auth_events
-        needed_auth_event_ids = set(event.auth_event_ids()) - set(batched_auth_events)
+        needed_auth_event_ids = [event_id for event_id in event.auth_event_ids() if event_id not in batched_auth_events]
         if needed_auth_event_ids:
             needed_auth_events = await store.get_events(
                 needed_auth_event_ids,
