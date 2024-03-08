31bf88b80a25b96eec1845090eeb00cb1fabcbdb
Gerrit Gogel <gerrit@gogel.me>
2024-03-08 15:24:10 +0100

    add items individually

--- synapse/event_auth.py
+++ synapse/event_auth.py
@@ -186,14 +186,18 @@ async def check_state_independent_auth_rules(
         return
 
     # 2. Reject if event has auth_events that: ...
-    auth_events: MutableMapping[str, "EventBase"]
+    auth_events: MutableMapping[str, "EventBase"] = {}
     if batched_auth_events:
-        auth_events = batched_auth_events
+        # Add items individually to prevent locking the whole mapping
+        for key, value in batched_auth_events.items():
+            auth_events[key] = value
+        
         needed_auth_event_ids = [
             event_id
             for event_id in event.auth_event_ids()
             if event_id not in batched_auth_events
         ]
+
         if needed_auth_event_ids:
             needed_auth_events = await store.get_events(
                 needed_auth_event_ids,
