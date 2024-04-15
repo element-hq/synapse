656a007c701fec061e2e397051e16ce58905db92
Andrew Morgan <andrew@amorgan.xyz>
2024-04-15 15:56:09 +0100

    Revert "block non-admins from publishing to room directory"
    This reverts commit e34f406cfd1ded69fb5c7d66321631679dac7ff6.

--- synapse/rest/client/directory.py
+++ synapse/rest/client/directory.py
@@ -166,16 +166,6 @@ class ClientDirectoryListServer(RestServlet):
 
         content = parse_and_validate_json_object_from_request(request, self.PutBody)
 
-        # temporarily block publishing rooms to public directory for non-admins
-        # patch date 12/12/23
-        if content.visibility == "public":
-            is_admin = await self.auth.is_server_admin(requester)
-            if not is_admin:
-                raise AuthError(
-                    403,
-                    "Publishing rooms to the room list is temporarily disabled.",
-                )
-
         await self.directory_handler.edit_published_room_list(
             requester, room_id, content.visibility
         )
