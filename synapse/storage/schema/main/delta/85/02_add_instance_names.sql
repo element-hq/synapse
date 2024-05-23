386e3830277b95b71c9c17d743e89eeaf3023d17
Erik Johnston <erik@matrix.org>
2024-05-23 15:51:46 +0100

    Port push rules stream to use MultiWriterIdGenerator

--- synapse/storage/schema/main/delta/85/02_add_instance_names.sql
+++ synapse/storage/schema/main/delta/85/02_add_instance_names.sql
@@ -18,3 +18,5 @@ ALTER TABLE device_lists_changes_in_room ADD COLUMN instance_name TEXT;
 ALTER TABLE device_lists_remote_pending ADD COLUMN instance_name TEXT;
 
 ALTER TABLE e2e_cross_signing_keys ADD COLUMN instance_name TEXT;
+
+ALTER TABLE push_rules_stream ADD COLUMN instance_name TEXT;
