1ea6deccad10e31f959e76d3f20d4fc7dbeb62e6
Erik Johnston <erik@matrix.org>
2024-05-23 15:45:33 +0100

    Port cross signing keys to use MultiWriterIdGenerator

--- synapse/storage/schema/main/delta/85/02_add_instance_names.sql
+++ synapse/storage/schema/main/delta/85/02_add_instance_names.sql
@@ -16,3 +16,5 @@ ALTER TABLE user_signature_stream ADD COLUMN instance_name TEXT;
 ALTER TABLE device_lists_outbound_pokes ADD COLUMN instance_name TEXT;
 ALTER TABLE device_lists_changes_in_room ADD COLUMN instance_name TEXT;
 ALTER TABLE device_lists_remote_pending ADD COLUMN instance_name TEXT;
+
+ALTER TABLE e2e_cross_signing_keys ADD COLUMN instance_name TEXT;
