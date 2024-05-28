b50a8e25a4f109d18786b286ca3e124bc4ab046b
Erik Johnston <erik@matrix.org>
2024-05-28 09:58:03 +0100

    Change allow_unsafe_locale to also apply on new databases

--- synapse/storage/engines/postgres.py
+++ synapse/storage/engines/postgres.py
@@ -142,6 +142,10 @@ class PostgresEngine(
         apply stricter checks on new databases versus existing database.
         """
 
+        allow_unsafe_locale = self.config.get("allow_unsafe_locale", False)
+        if allow_unsafe_locale:
+            return
+
         collation, ctype = self.get_db_locale(txn)
 
         errors = []
@@ -154,8 +158,10 @@ class PostgresEngine(
 
         if errors:
             raise IncorrectDatabaseSetup(
-                "Database is incorrectly configured:\n\n%s\n\n"
-                "See docs/postgres.md for more information." % ("\n".join(errors))
+                "Database has incorrect ctype of %r. Should be 'C'\n"
+                "See docs/postgres.md for more information. You can override this check by"
+                "setting 'allow_unsafe_locale' to true in the database config.",
+                ctype,
             )
 
     def convert_param_style(self, sql: str) -> str:
