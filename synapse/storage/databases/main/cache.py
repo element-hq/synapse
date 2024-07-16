7ae24258b4815dac1761b84d2c7bf442e01572bd
Eric Eastwood <eric.eastwood@beta.gouv.fr>
2024-07-16 14:56:21 -0500

    Invalidate caches

--- synapse/storage/databases/main/cache.py
+++ synapse/storage/databases/main/cache.py
@@ -269,12 +269,18 @@ class CacheInvalidationWorkerStore(SQLBaseStore):
 
             if data.type == EventTypes.Member:
                 self.get_rooms_for_user.invalidate((data.state_key,))  # type: ignore[attr-defined]
+            elif data.type == EventTypes.RoomEncryption:
+                self.get_room_encryption.invalidate((data.room_id,))  # type: ignore[attr-defined]
+            elif data.type == EventTypes.Create:
+                self.get_room_type.invalidate((data.room_id,))  # type: ignore[attr-defined]
         elif row.type == EventsStreamAllStateRow.TypeId:
             assert isinstance(data, EventsStreamAllStateRow)
             # Similar to the above, but the entire caches are invalidated. This is
             # unfortunate for the membership caches, but should recover quickly.
             self._curr_state_delta_stream_cache.entity_has_changed(data.room_id, token)  # type: ignore[attr-defined]
             self.get_rooms_for_user.invalidate_all()  # type: ignore[attr-defined]
+            self.get_room_type.invalidate((data.room_id,))  # type: ignore[attr-defined]
+            self.get_room_encryption.invalidate((data.room_id,))  # type: ignore[attr-defined]
         else:
             raise Exception("Unknown events stream row type %s" % (row.type,))
 
@@ -342,6 +348,10 @@ class CacheInvalidationWorkerStore(SQLBaseStore):
             self._attempt_to_invalidate_cache(
                 "get_forgotten_rooms_for_user", (state_key,)
             )
+        elif etype == EventTypes.Create:
+            self._attempt_to_invalidate_cache("get_room_type", (room_id,))
+        elif etype == EventTypes.RoomEncryption:
+            self._attempt_to_invalidate_cache("get_room_encryption", (room_id,))
 
         if relates_to:
             self._attempt_to_invalidate_cache(
@@ -399,6 +409,8 @@ class CacheInvalidationWorkerStore(SQLBaseStore):
         self._attempt_to_invalidate_cache("get_thread_summary", None)
         self._attempt_to_invalidate_cache("get_thread_participated", None)
         self._attempt_to_invalidate_cache("get_threads", (room_id,))
+        self._attempt_to_invalidate_cache("get_room_type", (room_id,))
+        self._attempt_to_invalidate_cache("get_room_encryption", (room_id,))
 
         self._attempt_to_invalidate_cache("_get_state_group_for_event", None)
 
@@ -451,6 +463,8 @@ class CacheInvalidationWorkerStore(SQLBaseStore):
         self._attempt_to_invalidate_cache("get_forgotten_rooms_for_user", None)
         self._attempt_to_invalidate_cache("_get_membership_from_event_id", None)
         self._attempt_to_invalidate_cache("get_room_version_id", (room_id,))
+        self._attempt_to_invalidate_cache("get_room_type", (room_id,))
+        self._attempt_to_invalidate_cache("get_room_encryption", (room_id,))
 
         # And delete state caches.
 
