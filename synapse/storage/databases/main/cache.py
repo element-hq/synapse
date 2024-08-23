3e7ebc6b2b01b1415a90ffda8d749245d1485042
Erik Johnston <erik@matrix.org>
2024-08-23 15:49:15 +0100

    Speed up sort rooms via cache

--- synapse/storage/databases/main/cache.py
+++ synapse/storage/databases/main/cache.py
@@ -313,6 +313,8 @@ class CacheInvalidationWorkerStore(SQLBaseStore):
             "get_unread_event_push_actions_by_room_for_user", (room_id,)
         )
 
+        self._attempt_to_invalidate_cache("_get_max_event_pos", (room_id,))
+
         # The `_get_membership_from_event_id` is immutable, except for the
         # case where we look up an event *before* persisting it.
         self._attempt_to_invalidate_cache("_get_membership_from_event_id", (event_id,))
