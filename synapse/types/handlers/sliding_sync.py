5fe6466d60196ffff175c197e60e4332a13500af
Erik Johnston <erikj@element.io>
2024-08-27 12:05:35 +0100

    Update synapse/types/handlers/sliding_sync.py
    Co-authored-by: Eric Eastwood <eric.eastwood@beta.gouv.fr>

--- synapse/types/handlers/sliding_sync.py
+++ synapse/types/handlers/sliding_sync.py
@@ -523,7 +523,7 @@ class RoomSyncConfig:
         }
 
         # Take the highest timeline limit
-        if self.timeline_limit < other_room_sync_config.timeline_limit:
+        if timeline_limit < other_room_sync_config.timeline_limit:
             timeline_limit = other_room_sync_config.timeline_limit
 
         # Union the required state
