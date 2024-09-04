798c79e7735ee52c458938d2aa10a001745c127c
Andrew Ferrazzutti <andrewf@element.io>
2024-09-04 09:20:47 -0400

    Remove license headers on new files

--- synapse/rest/client/delayed_events.py
+++ synapse/rest/client/delayed_events.py
@@ -1,21 +1,3 @@
-#
-# This file is licensed under the Affero General Public License (AGPL) version 3.
-#
-# Copyright (C) 2024 New Vector, Ltd
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# See the GNU Affero General Public License for more details:
-# <https://www.gnu.org/licenses/agpl-3.0.html>.
-#
-# Originally licensed under the Apache License, Version 2.0:
-# <http://www.apache.org/licenses/LICENSE-2.0>.
-#
-#
-
 """ This module contains REST servlets to do with delayed events: /delayed_events/<paths> """
 import logging
 from enum import Enum
