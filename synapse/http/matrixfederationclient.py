d69604eca4160c2b9bdf3cdef13814ab4b4f61ab
Quentin Gliech <quenting@element.io>
2024-09-24 18:45:45 +0200

    Correct typing of a variable now that it is now Unknown

--- synapse/http/matrixfederationclient.py
+++ synapse/http/matrixfederationclient.py
@@ -792,7 +792,7 @@ class MatrixFederationHttpClient:
                                 url_str,
                                 _flatten_response_never_received(e),
                             )
-                            body = None
+                            body = b''
 
                         exc = HttpResponseException(
                             response.code, response_phrase, body
