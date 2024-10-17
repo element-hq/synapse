287f0a69be1a797b22dfa89c6004f4b99e84e558
Jason Little <realtyem@gmail.com>
2024-10-17 15:14:40 -0500

    Use superclass version of `executescript()` (#1)

--- synapse/storage/engines/psycopg2.py
+++ synapse/storage/engines/psycopg2.py
@@ -83,15 +83,3 @@ class Psycopg2Engine(
         else:
             isolation_level = self.isolation_level_map[isolation_level]
         return conn.set_isolation_level(isolation_level)
-
-    @staticmethod
-    def executescript(cursor: psycopg2.extensions.cursor, script: str) -> None:
-        """Execute a chunk of SQL containing multiple semicolon-delimited statements.
-
-        Psycopg2 seems happy to do this in DBAPI2's `execute()` function.
-
-        For consistency with SQLite, any ongoing transaction is committed before
-        executing the script in its own transaction. The script transaction is
-        left open and it is the responsibility of the caller to commit it.
-        """
-        cursor.execute(f"COMMIT; BEGIN TRANSACTION; {script}")
