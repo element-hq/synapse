ccb21582ed86404dfa3b20dac6f4aadba78af763
Devon Hudson <devonhudson@librem.one>
2025-02-13 17:09:59 -0700

    Try OVERRIDING SYSTEM VALUE

--- synapse/_scripts/synapse_port_db.py
+++ synapse/_scripts/synapse_port_db.py
@@ -271,7 +271,7 @@ class Store(
     def insert_many_txn(
         self, txn: LoggingTransaction, table: str, headers: List[str], rows: List[Tuple]
     ) -> None:
-        sql = "INSERT INTO %s (%s) VALUES (%s)" % (
+        sql = "INSERT INTO %s (%s) VALUES (%s) OVERRIDING SYSTEM VALUE" % (
             table,
             ", ".join(k for k in headers),
             ", ".join("%s" for _ in headers),
