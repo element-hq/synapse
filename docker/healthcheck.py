fd0aa3c24788510e44873529fb0226afbcca8e51
Andrew Ferrazzutti <andrewf@element.io>
2025-03-04 08:12:34 -0500

    Lint

--- docker/healthcheck.py
+++ docker/healthcheck.py
@@ -1,7 +1,8 @@
 #!/usr/local/bin/python
 import subprocess
 import sys
+
 try:
     subprocess.check_call(["curl", "-fSs", "http://localhost:8008/health"])
-except subprocess.CalledProcessError as e:
+except subprocess.CalledProcessError:
     sys.exit(1)
