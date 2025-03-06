fe507864573a945febf082eb23976ff7118e57f2
Andrew Ferrazzutti <andrewf@element.io>
2025-03-06 01:13:58 -0500

    Go back to using wget instead of curl
    Also add a comment explaining why it's used, and install it

--- docker/healthcheck.py
+++ docker/healthcheck.py
@@ -3,6 +3,6 @@ import subprocess
 import sys
 
 try:
-    subprocess.check_call(["curl", "-fSs", "http://localhost:8008/health"])
+    subprocess.check_call(["wget", "--quiet", "--tries=1", "--spider", "http://localhost:8008/health"])
 except subprocess.CalledProcessError:
     sys.exit(1)
