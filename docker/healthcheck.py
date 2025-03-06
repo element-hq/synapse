428e769230245a23e43008863f8a1c60713bb9f5
Andrew Ferrazzutti <andrewf@element.io>
2025-03-06 01:35:52 -0500

    Lint

--- docker/healthcheck.py
+++ docker/healthcheck.py
@@ -3,6 +3,8 @@ import subprocess
 import sys
 
 try:
-    subprocess.check_call(["wget", "--quiet", "--tries=1", "--spider", "http://localhost:8008/health"])
+    subprocess.check_call(
+        ["wget", "--quiet", "--tries=1", "--spider", "http://localhost:8008/health"]
+    )
 except subprocess.CalledProcessError:
     sys.exit(1)
