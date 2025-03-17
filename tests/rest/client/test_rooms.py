c7dc36cf587c9f40e519cc0e0d61d586fd213f8b
H. Shay <hillerys@element.io>
2025-03-17 12:52:26 -0700

    extra tests

--- tests/rest/client/test_rooms.py
+++ tests/rest/client/test_rooms.py
@@ -4289,3 +4289,82 @@ class RoomParticipantTestCase(unittest.HomeserverTestCase):
             self.store.get_room_participation(self.user2, self.room1)
         )
         self.assertTrue(participant)
+
+    def test_sending_message_and_leaving_does_not_record_participation(self) -> None:
+        """
+        Test that sending an m.room.message event into a room and then leaving
+        causes the user to no longer be marked as a participant in that room
+        """
+        self.helper.join(self.room1, self.user2, tok=self.tok2)
+
+        # user has not sent any messages, so should not be a participant
+        participant = self.get_success(
+            self.store.get_room_participation(self.user2, self.room1)
+        )
+        self.assertFalse(participant)
+
+        # sending a message should now mark user as participant
+        self.helper.send_event(
+            self.room1,
+            "m.room.message",
+            content={
+                "msgtype": "m.text",
+                "body": "I am engaging in this room",
+            },
+            tok=self.tok2,
+        )
+        participant = self.get_success(
+            self.store.get_room_participation(self.user2, self.room1)
+        )
+        self.assertTrue(participant)
+
+        # leave the room
+        self.helper.leave(self.room1, self.user2, tok=self.tok2)
+
+        # user should no longer be considered a participant
+        participant = self.get_success(
+            self.store.get_room_participation(self.user2, self.room1)
+        )
+        self.assertFalse(participant)
+
+    def test_sending_encrypted_event_and_leaving_does_not_record_participation(
+        self,
+    ) -> None:
+        """
+        Test that sending an m.room.encrypted event into a room and then leaving causes
+        the user to no longer be marked as a participant in that room
+        """
+        self.helper.join(self.room1, self.user2, tok=self.tok2)
+
+        # user has not sent any messages, so should not be a participant
+        participant = self.get_success(
+            self.store.get_room_participation(self.user2, self.room1)
+        )
+        self.assertFalse(participant)
+
+        # sending an encrypted event should now mark user as participant
+        self.helper.send_event(
+            self.room1,
+            "m.room.encrypted",
+            content={
+                "algorithm": "m.megolm.v1.aes-sha2",
+                "ciphertext": "AwgAEnACgAkLmt6qF84IK++J7UDH2Za1YVchHyprqTqsg...",
+                "device_id": "RJYKSTBOIE",
+                "sender_key": "IlRMeOPX2e0MurIyfWEucYBRVOEEUMrOHqn/8mLqMjA",
+                "session_id": "X3lUlvLELLYxeTx4yOVu6UDpasGEVO0Jbu+QFnm0cKQ",
+            },
+            tok=self.tok2,
+        )
+        participant = self.get_success(
+            self.store.get_room_participation(self.user2, self.room1)
+        )
+        self.assertTrue(participant)
+
+        # leave the room
+        self.helper.leave(self.room1, self.user2, tok=self.tok2)
+
+        # user should no longer be considered a participant
+        participant = self.get_success(
+            self.store.get_room_participation(self.user2, self.room1)
+        )
+        self.assertFalse(participant)
