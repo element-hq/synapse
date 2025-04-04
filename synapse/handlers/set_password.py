ced8f29f5d0a6b740f099e8f63338918a059dc39
Quentin Gliech <quenting@element.io>
2025-04-04 16:21:23 +0200

    Allow the SetPasswordHandler to be instantiated on workers when password changing is disabled

--- synapse/handlers/set_password.py
+++ synapse/handlers/set_password.py
@@ -36,10 +36,17 @@ class SetPasswordHandler:
     def __init__(self, hs: "HomeServer"):
         self.store = hs.get_datastores().main
         self._auth_handler = hs.get_auth_handler()
-        # This can only be instantiated on the main process.
-        device_handler = hs.get_device_handler()
-        assert isinstance(device_handler, DeviceHandler)
-        self._device_handler = device_handler
+
+        # We don't need the device handler if password changing is disabled.
+        # This allows us to instantiate the SetPasswordHandler on the workers
+        # that have admin APIs for MAS
+        if self._auth_handler.can_change_password():
+            # This can only be instantiated on the main process.
+            device_handler = hs.get_device_handler()
+            assert isinstance(device_handler, DeviceHandler)
+            self._device_handler: Optional[DeviceHandler] = device_handler
+        else:
+            self._device_handler = None
 
     async def set_password(
         self,
@@ -48,7 +55,8 @@ class SetPasswordHandler:
         logout_devices: bool,
         requester: Optional[Requester] = None,
     ) -> None:
-        if not self._auth_handler.can_change_password():
+        # If the device_handler is None, then password changing is disabled.
+        if self._device_handler is None:
             raise SynapseError(403, "Password change disabled", errcode=Codes.FORBIDDEN)
 
         try:
