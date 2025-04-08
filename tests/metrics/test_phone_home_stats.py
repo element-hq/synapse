0a06be3a1434eeb5ba4911b72e0fdd6619e2cf4e
Eric Eastwood <erice@element.io>
2025-04-08 15:14:59 -0500

    Fix lints

--- tests/metrics/test_phone_home_stats.py
+++ tests/metrics/test_phone_home_stats.py
@@ -23,7 +23,6 @@ from synapse.app.phone_stats_home import (
 from synapse.rest import admin, login, register, room
 from synapse.server import HomeServer
 from synapse.types import JsonDict
-from synapse.types.storage import _BackgroundUpdates
 from synapse.util import Clock
 
 from tests import unittest
