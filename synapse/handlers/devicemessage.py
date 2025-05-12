35d98b66c70241409356c732b8d7b4fd7ebabe97
Mathieu Velten <mathieu.velten@beta.gouv.fr>
2025-05-12 21:18:23 +0200

    Improve logs

--- synapse/handlers/devicemessage.py
+++ synapse/handlers/devicemessage.py
@@ -469,7 +469,7 @@ def get_device_message_edu_contents(
         if current_edu_size + message_entry_size > MAX_EDU_SIZE:
             edu_contents.append(current_edu_content)
             logger.debug(
-                "Splitting %d device messages from %s into a separate EDU msgid %s, %d EDUs queued",
+                "Splitting %d device messages from %s into EDU msgid %s, %d EDUs queued",
                 len(current_edu_content["messages"]),
                 sender_user_id,
                 current_edu_content["message_id"],
