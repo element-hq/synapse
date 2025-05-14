3a38bd80c6884f5d6fd3bb725a9f8a0618ce624c
Erik Johnston <erik@matrix.org>
2025-05-14 16:17:08 +0100

    Remove test file

--- synapse/storage/schema/main/delta/92/05_test.sql
+++
@@ -1,5 +0,0 @@
--- This is a test to ensure that we don't run
--- CREATE INDEX or DROP INDEX in the schema file, and instead use
--- background updates.
-
-CREATE UNIQUE index foobar ON foobar(ads);
