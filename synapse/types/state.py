404fc7b3b5643dba822ea71789fa1ccb6868239a
Andrew Morgan <andrew@amorgan.xyz>
2025-05-19 17:43:59 +0100

    Run `ruff check --fix .`

--- synapse/types/state.py
+++ synapse/types/state.py
@@ -462,7 +462,7 @@ class StateFilter:
             new_types.update({state_type: set() for state_type in minus_wildcards})
 
         # insert the plus wildcards
-        new_types.update({state_type: None for state_type in plus_wildcards})
+        new_types.update(dict.fromkeys(plus_wildcards))
 
         # insert the specific state keys
         for state_type, state_key in plus_state_keys:
