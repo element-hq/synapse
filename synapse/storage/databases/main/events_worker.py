827b78554f32028a00fc7bc9ae91bc3a9eb25763
Eric Eastwood <erice@element.io>
2025-05-21 09:28:27 -0500

    Reapply "WIP: `@cached` progress"
    This reverts commit b4ed25e4fb03e94ce18cb60b4f3f9464bfee8964.

--- synapse/storage/databases/main/events_worker.py
+++ synapse/storage/databases/main/events_worker.py
@@ -286,8 +286,7 @@ class EventsWorkerStore(SQLBaseStore):
             AsyncLruCache(
                 max_size=hs.config.caches.event_cache_size,
                 cache_name="*getEvent*",
-                # TODO
-                # cache_manager=hs.get_cache_manager(),
+                cache_manager=hs.get_cache_manager(),
                 # `extra_index_cb` Returns a tuple as that is the key type
                 extra_index_cb=lambda _, v: (v.event.room_id,),
             )
