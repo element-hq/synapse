de55ace8bce5b6f452d6431c1fe6e824d0408961
Half-Shot <will@half-shot.uk>
2025-05-30 08:32:02 +0100

    Refactor so that rules are processed in the correct order

--- synapse/storage/invite_rule.py
+++ synapse/storage/invite_rule.py
@@ -2,7 +2,6 @@ import logging
 from enum import Enum
 from typing import Optional, Pattern
 
-from immutabledict import ImmutableOrderedDict
 from matrix_common.regex import glob_to_regex
 
 from synapse.types import JsonMapping, UserID
@@ -21,59 +20,82 @@ class InviteRule(Enum):
 class InviteRulesConfig:
     """Class to determine if a given user permits an invite from another user, and the action to take."""
 
-    user_rules: ImmutableOrderedDict[Pattern[str], InviteRule]
-    server_rules: ImmutableOrderedDict[Pattern[str], InviteRule]
-
     def __init__(self, account_data: Optional[JsonMapping]):
-        user_rules: dict[Pattern[str], InviteRule] = {}
-        server_rules: dict[Pattern[str], InviteRule] = {}
+        self.allowed_users: list[Pattern[str]] = []
+        self.ignored_users: list[Pattern[str]] = []
+        self.blocked_users: list[Pattern[str]] = []
+
+        self.allowed_servers: list[Pattern[str]] = []
+        self.ignored_servers: list[Pattern[str]] = []
+        self.blocked_servers: list[Pattern[str]] = []
 
         def process_field(
             values: Optional[list[str]],
-            ruleset: dict[Pattern[str], InviteRule],
+            ruleset: list[Pattern[str]],
             rule: InviteRule,
         ) -> None:
             if isinstance(values, list):
                 for value in values:
                     if isinstance(value, str) and len(value) >= 1:
                         try:
-                            ruleset[glob_to_regex(value)] = rule
+                            ruleset.append(glob_to_regex(value))
                         except Exception as e:
                             # If for whatever reason we can't process this, just ignore it.
                             logger.debug("Could not process rule '%s': %s", value, e)
 
         if account_data:
-            # In reverse order of importance.
             process_field(
-                account_data.get("blocked_users"), user_rules, InviteRule.BLOCK
+                account_data.get("allowed_users"), self.allowed_users, InviteRule.ALLOW
             )
             process_field(
-                account_data.get("ignored_users"), user_rules, InviteRule.IGNORE
+                account_data.get("ignored_users"), self.ignored_users, InviteRule.IGNORE
             )
             process_field(
-                account_data.get("allowed_users"), user_rules, InviteRule.ALLOW
+                account_data.get("blocked_users"), self.blocked_users, InviteRule.BLOCK
             )
             process_field(
-                account_data.get("blocked_servers"), server_rules, InviteRule.BLOCK
+                account_data.get("allowed_servers"),
+                self.allowed_servers,
+                InviteRule.ALLOW,
             )
             process_field(
-                account_data.get("ignored_servers"), server_rules, InviteRule.IGNORE
+                account_data.get("ignored_servers"),
+                self.ignored_servers,
+                InviteRule.IGNORE,
             )
             process_field(
-                account_data.get("allowed_servers"), server_rules, InviteRule.ALLOW
+                account_data.get("blocked_servers"),
+                self.blocked_servers,
+                InviteRule.BLOCK,
             )
-
-        self.user_rules = ImmutableOrderedDict(user_rules)
-        self.server_rules = ImmutableOrderedDict(server_rules)
 
     def get_invite_rule(self, user_id: UserID) -> InviteRule:
         """Get the invite rule that matches this user. Will return InviteRule.ALLOW if no rules match"""
-        for regex, user_rule in self.user_rules.items():
-            if regex.match(user_id.to_string()):
-                return user_rule
+        user_id_str = user_id.to_string()
+        # The order here is important. We always process user rules before server rules
+        # and we always process in the order of Allow, Ignore, Block.
+        for regex in self.allowed_users:
+            if regex.match(user_id_str):
+                return InviteRule.ALLOW
 
-        for regex, server_rule in self.server_rules.items():
+        for regex in self.ignored_users:
+            if regex.match(user_id_str):
+                return InviteRule.IGNORE
+
+        for regex in self.blocked_users:
+            if regex.match(user_id_str):
+                return InviteRule.BLOCK
+
+        for regex in self.allowed_servers:
             if regex.match(user_id.domain):
-                return server_rule
+                return InviteRule.ALLOW
+
+        for regex in self.ignored_servers:
+            if regex.match(user_id.domain):
+                return InviteRule.IGNORE
+
+        for regex in self.blocked_servers:
+            if regex.match(user_id.domain):
+                return InviteRule.BLOCK
 
         return InviteRule.ALLOW
