dac68a3bdd6da0a0654970a98b6ac51e648bea5e
turt2live <1190097+turt2live@users.noreply.github.com>
2025-06-20 19:07:03 +0000

    Attempt to fix linting

--- synapse/visibility.py
+++ synapse/visibility.py
@@ -117,7 +117,10 @@ async def filter_events_for_client(
     client_config = await storage.main.get_admin_client_config_for_user(user_id)
     if (
         filter_send_to_client
-        and (client_config.return_soft_failed_events or client_config.return_policy_server_spammy_events)
+        and (
+            client_config.return_soft_failed_events
+            or client_config.return_policy_server_spammy_events
+        )
         and await storage.main.is_server_admin(UserID.from_string(user_id))
     ):
         if client_config.return_policy_server_spammy_events:
