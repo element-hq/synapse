1b03330790d55521e4d742a787cc4d32adde1e2a
Eric Eastwood <erice@element.io>
2025-07-23 14:27:02 -0500

    Fix `make_request(...)` in tests

--- tests/server.py
+++ tests/server.py
@@ -432,7 +432,7 @@ def make_request(
 
     channel = FakeChannel(site, reactor, ip=client_ip)
 
-    req = request(channel, site)
+    req = request(channel, site, our_server_name="test_server")
     channel.request = req
 
     req.content = BytesIO(content)
