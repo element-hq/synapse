a2ba909ded48a4880ee0e40e473e3022f5f30f6d
reivilibre <oliverw@element.io>
2025-07-30 12:30:40 +0100

    Remove obsolete `/send_event` replication endpoint. (#18730)
    Fixes: #18441
    
    Signed-off-by: Olivier 'reivilibre <oliverw@matrix.org>

--- synapse/replication/http/__init__.py
+++ synapse/replication/http/__init__.py
@@ -31,7 +31,6 @@ from synapse.replication.http import (
     presence,
     push,
     register,
-    send_event,
     send_events,
     state,
     streams,
@@ -50,7 +49,6 @@ class ReplicationRestResource(JsonResource):
         self.register_servlets(hs)
 
     def register_servlets(self, hs: "HomeServer") -> None:
-        send_event.register_servlets(hs, self)
         send_events.register_servlets(hs, self)
         federation.register_servlets(hs, self)
         presence.register_servlets(hs, self)
