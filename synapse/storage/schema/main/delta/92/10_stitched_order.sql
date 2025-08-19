2e3e95839529cc037e389b33aa74bef8e3112bd6
Richard van der Hoff <richard@matrix.org>
2025-08-19 18:51:16 +0100

    Assign ordering to backward extremities
    We're going to use backward extremities to represent "gaps" in our
    algorithm. (Specifically, a "gap" is represented by a set of backward
    extremities which all appear at the same point in the stitched ordering).
    
    Accordingly, we need to assign them a stitched ordering. We're going to do this
    indirectly, by referencing the event that appears before it in the stitched
    ordering, so that there is only one table that has a stitched ordering column.

--- synapse/storage/schema/main/delta/92/10_stitched_order.sql
+++ synapse/storage/schema/main/delta/92/10_stitched_order.sql
@@ -14,3 +14,27 @@
 ALTER TABLE events ADD COLUMN stitched_ordering BIGINT;
 CREATE UNIQUE INDEX events_stitched_order ON events(room_id, stitched_ordering); -- TODO make this concurrent
 
+--CREATE TABLE stitched_ordering_gaps (
+--    room_id TEXT NOT NULL,
+--    following_event_id TEXT NOT NULL,
+--    missing_event_id TEXT NOT NULL,
+--    UNIQUE (room_id, following_event_id, missing_event_id)
+--);
+--CREATE INDEX stitched_ordering_gaps_missing_events ON stitched_ordering_gaps(room_id, missing_event_id);
+
+-- Gaps in the stitched ordering are equivalent to a group of backward extremities that appear at
+-- the same point in the stitched ordering.
+--
+-- Rather than explicitly tracking where in the stitched ordering a given gap appears, we record the
+-- event id of the event that comes *before* the gap in the stitched ordering. Doing so means that:
+--
+--   1. There is only one table that has a `stitched_ordering` column, making it easier to figure out
+--      how to insert a batch of events between existing events (and making the UNIQUE constraint effective).
+--
+--   2. We don't need to allocate space in the stitched ordering for gaps; in particular we can assign an order
+--      to gaps *after* we have persisted the events. (We could probably work around this by double-spacing inserted
+--      events? but still, it's a nice property)
+--
+-- Note that this assumes that we never need to insert an event *before* a gap (or if we did,
+-- we'd have to update this table).
+ALTER TABLE event_backward_extremities ADD COLUMN before_gap_event_id TEXT REFERENCES events (event_id);
