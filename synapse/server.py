ca655e4020f7970a525fe76d95f7aef259dfc1a9
Eric Eastwood <erice@element.io>
2025-09-09 10:10:34 -0500

    Start background tasks after we fork the process (daemonize) (#18886)
    Spawning from https://github.com/element-hq/synapse/pull/18871
    
    [This change](https://github.com/element-hq/synapse/pull/18871/commits/6ce2f3e59d6dcd8a798552a087414cf137d0ddad)
    was originally used to fix CPU time going backwards when we `daemonize`.
    
    While, we don't seem to run into this problem on `develop`, I still
    think this is a good change to make. We don't need background tasks
    running on a process that will soon be forcefully exited and where the
    reactor isn't even running yet. We now kick off the background tasks
    (`run_as_background_process`) after we have forked the process and
    started the reactor.
    
    Also as simple note, we don't need background tasks running in both halves of a fork.

--- synapse/server.py
+++ synapse/server.py
@@ -366,12 +366,6 @@ class HomeServer(metaclass=abc.ABCMeta):
         self.datastores = Databases(self.DATASTORE_CLASS, self)
         logger.info("Finished setting up.")
 
-        # Register background tasks required by this server. This must be done
-        # somewhat manually due to the background tasks not being registered
-        # unless handlers are instantiated.
-        if self.config.worker.run_background_tasks:
-            self.setup_background_tasks()
-
     def __del__(self) -> None:
         """
         Called when an the homeserver is garbage collected.
@@ -410,7 +404,7 @@ class HomeServer(metaclass=abc.ABCMeta):
         appropriate listeners.
         """
 
-    def setup_background_tasks(self) -> None:
+    def start_background_tasks(self) -> None:
         """
         Some handlers have side effects on instantiation (like registering
         background updates). This function causes them to be fetched, and
