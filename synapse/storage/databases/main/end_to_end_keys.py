e2d9d3450d38d4d4cd287ea88e5b2ad61b319f5c
Andrew Morgan <andrew@amorgan.xyz>
2025-09-23 15:46:39 +0100

    add comment about batch size origin

--- synapse/storage/databases/main/end_to_end_keys.py
+++ synapse/storage/databases/main/end_to_end_keys.py
@@ -354,6 +354,9 @@ class EndToEndKeyWorkerStore(EndToEndKeyBackgroundStore, CacheInvalidationWorker
             if d is not None and d.keys is not None
         )
 
+        # 1000 is an arbitrary batch size. It helped performance on a very
+        # large-scale deployment (matrix.org), but has not been tested against
+        # any other setup.
         for batch in batch_iter(signature_query, 1000):
             cross_sigs_result = (
                 await self._get_e2e_cross_signing_signatures_for_devices(batch)
