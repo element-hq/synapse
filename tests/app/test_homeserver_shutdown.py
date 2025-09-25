30dbc2b55c2b660779360c4cc5ff45902684fc69
Devon Hudson <devonhudson@librem.one>
2025-09-25 15:12:05 -0600

    Add shutdown test where background updates haven't completed

--- tests/app/test_homeserver_shutdown.py
+++ tests/app/test_homeserver_shutdown.py
@@ -22,6 +22,7 @@ import gc
 import weakref
 
 from synapse.app.homeserver import SynapseHomeServer
+from synapse.util.clock import CALL_LATER_DELAY_TRACKING_THRESHOLD_S
 
 from tests.server import get_clock, setup_test_homeserver
 from tests.unittest import HomeserverTestCase
@@ -52,11 +53,11 @@ class HomeserverCleanShutdownTestCase(HomeserverTestCase):
 
         hs_ref = weakref.ref(self.hs)
 
-        # Cleanup the homeserver.
-        self.get_success(self.hs.shutdown())
-
         # Run the reactor so any `callWhenRunning` functions can be cleared out.
         self.reactor.run()
+
+        # Cleanup the homeserver.
+        self.get_success(self.hs.shutdown())
 
         # Cleanup the internal reference in our test case
         del self.hs
@@ -64,6 +65,88 @@ class HomeserverCleanShutdownTestCase(HomeserverTestCase):
         # Advance the reactor to allow for any outstanding calls to be run.
         # A value of 1 is not enough, so a value of 2 is used.
         self.reactor.advance(2)
+
+        # Force garbage collection.
+        gc.collect()
+
+        # Ensure the `HomeServer` hs been garbage collected by attempting to use the
+        # weakref to it.
+        if hs_ref() is not None:
+            self.fail("HomeServer reference should not be valid at this point")
+
+            # To help debug this test when it fails, it is useful to leverage the
+            # `objgraph` module.
+            # The following code serves as an example of what I have found to be useful
+            # when tracking down references holding the `SynapseHomeServer` in memory:
+            #
+            # all_objects = gc.get_objects()
+            # for obj in all_objects:
+            #     try:
+            #         # These are a subset of types that are typically involved with
+            #         # holding the `HomeServer` in memory. You may want to inspect
+            #         # other types as well.
+            #         if isinstance(obj, DataStore):
+            #             print(sys.getrefcount(obj), "refs to", obj)
+            #             if not isinstance(obj, weakref.ProxyType):
+            #                 db_obj = obj
+            #         if isinstance(obj, SynapseHomeServer):
+            #             print(sys.getrefcount(obj), "refs to", obj)
+            #             if not isinstance(obj, weakref.ProxyType):
+            #                 synapse_hs = obj
+            #         if isinstance(obj, SynapseSite):
+            #             print(sys.getrefcount(obj), "refs to", obj)
+            #             if not isinstance(obj, weakref.ProxyType):
+            #                 sysite = obj
+            #         if isinstance(obj, DatabasePool):
+            #             print(sys.getrefcount(obj), "refs to", obj)
+            #             if not isinstance(obj, weakref.ProxyType):
+            #                 dbpool = obj
+            #     except Exception:
+            #         pass
+            #
+            # print(sys.getrefcount(hs_ref()), "refs to", hs_ref())
+            #
+            # # The following values for `max_depth` and `too_many` have been found to
+            # # render a useful amount of information without taking an overly long time
+            # # to generate the result.
+            # objgraph.show_backrefs(synapse_hs, max_depth=10, too_many=10)
+
+    def test_clean_homeserver_shutdown_mid_background_updates(self) -> None:
+        """Ensure the `SynapseHomeServer` can be fully shutdown and garbage collected
+        before background updates have completed"""
+        self.reactor, self.clock = get_clock()
+        self.hs = setup_test_homeserver(
+            self.addCleanup,
+            reactor=self.reactor,
+            homeserver_to_use=SynapseHomeServer,
+            clock=self.clock,
+        )
+
+        # Pump the background updates by a single iteration, just to ensure any extra
+        # resources it uses have been started.
+        store = weakref.proxy(self.hs.get_datastores().main)
+        self.get_success(store.db_pool.updates.do_next_background_update(False), by=0.1)
+
+        hs_ref = weakref.ref(self.hs)
+
+        # Run the reactor so any `callWhenRunning` functions can be cleared out.
+        self.reactor.run()
+        # Also advance the reactor by the delay tracking threshold to ensure all
+        # cancellable delayed calls have been scheduled. Must be done prior to
+        # `hs.shutdown()` otherwise they will be scheduled later during the test when we
+        # advance the reactor to wait out any non-tracked delayed calls.
+        self.reactor.advance(CALL_LATER_DELAY_TRACKING_THRESHOLD_S)
+
+        # Cleanup the homeserver.
+        self.get_success(self.hs.shutdown())
+
+        # Cleanup the internal reference in our test case
+        del self.hs
+
+        # Advance the reactor to allow for any outstanding calls to be run.
+        # The limit of 15 is currently set by having to wait for the delayed call
+        # `synapse.storage.database.py -> _check_safe_to_upsert`.
+        self.reactor.advance(15)
 
         # Force garbage collection.
         gc.collect()
