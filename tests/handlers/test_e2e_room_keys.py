af6dbad38aca95b260cf844d4fa4f05c4cf30e70
Eric Eastwood <erice@element.io>
2025-09-29 17:04:18 -0500

    Remove unused mock

--- tests/handlers/test_e2e_room_keys.py
+++ tests/handlers/test_e2e_room_keys.py
@@ -50,7 +50,7 @@ room_keys = {
 
 class E2eRoomKeysHandlerTestCase(unittest.HomeserverTestCase):
     def make_homeserver(self, reactor: MemoryReactor, clock: Clock) -> HomeServer:
-        return self.setup_test_homeserver(replication_layer=mock.Mock())
+        return self.setup_test_homeserver()
 
     def prepare(self, reactor: MemoryReactor, clock: Clock, hs: HomeServer) -> None:
         self.handler = hs.get_e2e_room_keys_handler()
