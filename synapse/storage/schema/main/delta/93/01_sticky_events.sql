4def40414e29c5d706cb86df38ae7448b8fae09e
Kegan Dougal <7190048+kegsay@users.noreply.github.com>
2025-10-06 14:45:14 +0100

    Appease the regexp on delta lint checks

--- synapse/storage/schema/main/delta/93/01_sticky_events.sql
+++ synapse/storage/schema/main/delta/93/01_sticky_events.sql
@@ -11,7 +11,7 @@
 -- See the GNU Affero General Public License for more details:
 -- <https://www.gnu.org/licenses/agpl-3.0.html>.
 
-CREATE TABLE IF NOT EXISTS sticky_events(
+CREATE TABLE sticky_events (
   stream_id INTEGER NOT NULL PRIMARY KEY,
   instance_name TEXT NOT NULL,
   room_id TEXT NOT NULL,
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS sticky_events(
 );
 
 -- for pulling out soft failed events by room
-CREATE INDEX IF NOT EXISTS sticky_events_room_idx ON sticky_events(room_id, soft_failed);
+CREATE INDEX sticky_events_room_idx ON sticky_events (room_id, soft_failed);
 
 -- A optional int for combining sticky events with delayed events. Used at send time.
 ALTER TABLE delayed_events ADD COLUMN sticky_duration_ms BIGINT;
