0ae1f105b22e51802b01f60ef5a386d8b27d7079
Andrew Morgan <1342360+anoadragon453@users.noreply.github.com>
2025-10-07 16:23:55 +0100

    Update `KeyUploadServlet` to handle case where client sends `device_keys: null` (#19023)

--- tests/rest/client/test_keys.py
+++ tests/rest/client/test_keys.py
@@ -160,6 +160,26 @@ class KeyUploadTestCase(unittest.HomeserverTestCase):
             channel.result,
         )
 
+    def test_upload_keys_succeeds_when_fields_are_explicitly_set_to_null(self) -> None:
+        """
+        This is a regression test for https://github.com/element-hq/synapse/pull/19023.
+        """
+        device_id = "DEVICE_ID"
+        self.register_user("alice", "wonderland")
+        alice_token = self.login("alice", "wonderland", device_id=device_id)
+
+        channel = self.make_request(
+            "POST",
+            "/_matrix/client/v3/keys/upload",
+            {
+                "device_keys": None,
+                "one_time_keys": None,
+                "fallback_keys": None,
+            },
+            alice_token,
+        )
+        self.assertEqual(channel.code, HTTPStatus.OK, channel.result)
+
 
 class KeyQueryTestCase(unittest.HomeserverTestCase):
     servlets = [
