1401a7d9e0fb5f0df887da0dfaeb3a7b897f0c3e
Eric Eastwood <erice@element.io>
2025-11-25 15:08:48 -0600

    Serialize token for easy ctrl+f in the next request

--- synapse/rest/client/room.py
+++ synapse/rest/client/room.py
@@ -916,7 +916,7 @@ class RoomMessageListRestServlet(RestServlet):
             request.get_method(),
             request.get_redacted_uri(),
             len(get_messages_result.messages_chunk),
-            get_messages_result.end_token,
+            await get_messages_result.end_token.to_string(self.store),
         )
 
         response_content = await self.encode_response(
