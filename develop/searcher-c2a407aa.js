d915fe87bf791b471826e62ee3053ffb565be952
devonh <devonh@users.noreply.github.com>
2026-01-27 23:10:09 +0000

    deploy: ede0f4f56b1b2e2bf66565c0b4d7990bd028c301

--- develop/searcher-c2a407aa.js
+++ develop/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-8a203ec6.js',
+                path_to_root + 'searchindex-723e99dc.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
