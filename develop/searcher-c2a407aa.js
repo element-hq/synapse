3a56d7b7a50f3095d543c3d1f997733d6e54c16b
MadLittleMods <MadLittleMods@users.noreply.github.com>
2026-03-03 16:06:43 +0000

    deploy: 51048b8e36e926a9151f1488ed0aca88d305b4d8

--- develop/searcher-c2a407aa.js
+++ develop/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-deb51e5a.js',
+                path_to_root + 'searchindex-b05bf627.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
