9a60073c14c5f9860c578ca2a3842fd7b0de84e4
turt2live <1190097+turt2live@users.noreply.github.com>
2026-03-04 21:55:51 +0000

    Attempt to fix linting

--- synapse/handlers/room_policy.py
+++ synapse/handlers/room_policy.py
@@ -218,7 +218,9 @@ class RoomPolicyHandler:
         # Note: if the policy server and event sender are the same server, the returned
         # signatures from the policy server might not contain the actual event signature.
         # This is why we go out of our way to add defaults.
-        event.signatures.setdefault(policy_server, {}).update(signature.get(policy_server, {}))
+        event.signatures.setdefault(policy_server, {}).update(
+            signature.get(policy_server, {})
+        )
         if verify:
             is_valid = await self._verify_policy_server_signature(
                 event, policy_server, public_key
