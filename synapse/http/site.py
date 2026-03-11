a5c38f46bf43d87f8a2368dabb55888058a8e76c
Eric Eastwood <erice@element.io>
2026-03-11 09:55:20 -0500

    Add some light labels to the `Processed request` logs

--- synapse/http/site.py
+++ synapse/http/site.py
@@ -641,7 +641,7 @@ class SynapseRequest(Request):
         self.synapse_site.access_logger.log(
             log_level,
             "%s - %s - {%s}"
-            " Processed request: %.3fsec/%.3fsec (%.3fsec, %.3fsec) (%.3fsec/%.3fsec/%d)"
+            " Processed request: %.3fsec/%.3fsec ru=(%.3fsec, %.3fsec) db=(%.3fsec/%.3fsec/%d)"
             ' %sB %s "%s %s %s" "%s" [%d dbevts]',
             self.get_client_ip_if_available(),
             self.synapse_site.site_tag,
