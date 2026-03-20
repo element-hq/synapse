52a79591d5e189dfc4dec90fbbaf2cf7f0d4aa4d
Erik Johnston <erik@matrix.org>
2026-03-20 09:10:14 +0000

    Fix licensing header

--- synapse/storage/schema/main/delta/94/02_redactions_recheck_bg_update.sql
+++ synapse/storage/schema/main/delta/94/02_redactions_recheck_bg_update.sql
@@ -1,7 +1,7 @@
 --
 -- This file is licensed under the Affero General Public License (AGPL) version 3.
 --
--- Copyright (C) 2026 New Vector, Ltd
+-- Copyright (C) 2026 Element Creations, Ltd
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU Affero General Public License as
