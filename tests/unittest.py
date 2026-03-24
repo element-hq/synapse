0a432d9a14dda1008435572b1717a157f39d8cc2
Eric Eastwood <erice@element.io>
2026-03-24 13:57:58 -0500

    Use "steps" to better illustrate discrete increments
    See https://github.com/element-hq/synapse/pull/19602#discussion_r2983564328

--- tests/unittest.py
+++ tests/unittest.py
@@ -704,8 +704,8 @@ class HomeserverTestCase(TestCase):
 
         To demystify this function, it simply advances time by the number of seconds
         specified (defaults to `0`, we also multiply by 100, so `pump(1)` is 100 seconds
-        in 1 second increments) which allows any queued/pending tasks to run because
-        enough time has passed.
+        in 1 second steps/increments) which allows any queued/pending tasks to run
+        because enough time has passed.
 
         So for example, if you have some Synapse code that does
         `clock.call_later(Duration(seconds=2), callback)`, then calling
@@ -727,11 +727,12 @@ class HomeserverTestCase(TestCase):
 
         Args:
             by: The time increment in seconds to advance time by. We will advance time
-                100x by this value.
+                in 100 steps, each step by this value.
         """
         # We multiply by 100, so `pump(1)` actually advances time by 100 seconds in 1
-        # second increments. We assume this was done so that when you create a Deferred
-        # that create more Deferreds, it tries to run the whole chain to completion.
+        # second steps/increments. We assume this was done so that when you create a
+        # Deferred that create more Deferreds, it tries to run the whole chain to
+        # completion.
         self.reactor.pump([by] * 100)
 
     def get_success(self, d: Awaitable[TV], by: float = 0.0) -> TV:
