b9699c1dca06288795ae5959e4ab7aca54f3282e
sandhose <sandhose@users.noreply.github.com>
2026-03-24 14:55:08 +0000

    deploy: 8c3daff2441473888953b8b69f969c5b248e68c9

--- develop/searcher-c2a407aa.js
+++ develop/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-26e57b9e.js',
+                path_to_root + 'searchindex-b358e032.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
