abda8899fa8009ddf48aed7db2b1eac27cc6fcc5
Gaëtan <166950915+gaetan-sbt@users.noreply.github.com>
2026-04-30 11:19:56 +0100

    Update tests/events/test_validator.py
    Co-authored-by: Eric Eastwood <madlittlemods@gmail.com>

--- tests/events/test_validator.py
+++ tests/events/test_validator.py
@@ -1,6 +1,8 @@
 #
 # This file is licensed under the Affero General Public License (AGPL) version 3.
 #
+# Copyright (C) 2026 Element Creations Ltd
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
