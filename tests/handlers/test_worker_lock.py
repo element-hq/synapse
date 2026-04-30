74f69d53ed18a317b9af0e4c7f83117dc7569404
Jason Little <j.little@famedly.com>
2026-04-30 16:06:58 -0500

    adjust timeout tests

--- tests/handlers/test_worker_lock.py
+++ tests/handlers/test_worker_lock.py
@@ -26,7 +26,9 @@ from unittest.mock import patch
 from twisted.internet import defer
 from twisted.internet.testing import MemoryReactor
 
+from synapse.handlers.worker_lock import WORKER_LOCK_MAX_RETRY_INTERVAL
 from synapse.server import HomeServer
+from synapse.storage.databases.main.lock import _LOCK_TIMEOUT_MS
 from synapse.util.clock import Clock
 
 from tests import unittest
@@ -41,6 +43,7 @@ class WorkerLockTestCase(unittest.HomeserverTestCase):
         self, reactor: MemoryReactor, clock: Clock, homeserver: HomeServer
     ) -> None:
         self.worker_lock_handler = self.hs.get_worker_locks_handler()
+        self.store = self.hs.get_datastores().main
 
     def test_wait_for_lock_locally(self) -> None:
         """Test waiting for a lock on a single worker"""
@@ -63,21 +66,59 @@ class WorkerLockTestCase(unittest.HomeserverTestCase):
         self.get_success(lock1.__aenter__())
 
         lock2 = self.worker_lock_handler.acquire_lock("name", "key")
-        # Wrap the WaitingLock object, so we can detect if the timeouts are being hit
+
+        d2 = defer.ensureDeferred(lock2.__aenter__())
+
+        self.assertNoResult(d2)
+
+        # Wrap the database call to attempt to acquire the lock, so we can detect how
+        # many times it is called on. With lock1 having already been entered, any other
+        # calls to try_acquire_lock() should only be for lock2
         with patch.object(
-            lock2,
-            "_increment_timeout_interval",
-            wraps=lock2._increment_timeout_interval,
-        ) as wrapped_lock2_increment_timeout_interval_method:
-            d2 = defer.ensureDeferred(lock2.__aenter__())
-            self.assertNoResult(d2)
+            self.store,
+            "try_acquire_lock",
+            wraps=self.store.try_acquire_lock,
+        ) as wrapped_try_acquire_lock_method:
+            # A Lock has an internal background looping call that runs every 30 seconds
+            # to renew the Lock and push it's "drop timeout" value further out by 2
+            # minutes. The Lock will prematurely drop if this renewal is not allowed to
+            # run, which sours the test.
 
-            # Recall that pump() will advance time of the given amount 100 times, this
-            # amounts to about 10 seconds passing
-            self.pump(0.1)
+            # Advance time by a bit over 3 hours. _LOCK_TIMEOUT_MS is 2 minutes. Remove
+            # 1 second from that to give it the barest minimum time to still renew
+            # itself(the test fails if we do not remove that second and the Lock will
+            # drop)
+            # 2 * 60 = 120 seconds
+            # 120 - 1 = 119 seconds to actually advance
+            pump_fraction = (_LOCK_TIMEOUT_MS / 1000) - 1
+            # 119 * 100 = 11_900 seconds for the entire pump call
+            # 11_900 / 60 = 180 minutes and 18.33 seconds
+            self.pump(pump_fraction)  # iterates 100 times, see the math above
+            # The actual Lock should not exist still
+            assert lock2._inner_lock is None
 
-            # Should be timed out 6 times, but do not fail on that exact count
-            wrapped_lock2_increment_timeout_interval_method.assert_called()
+            wrapped_try_acquire_lock_method.reset_mock()
+
+            # By this point, the timeout on the WaitingLock should be maxed out at
+            # WORKER_LOCK_MAX_RETRY_INTERVAL. Wait twice that long using pump() so lock1
+            # doesn't drop in the background causing an incorrect pass for the test.
+            # WORKER_LOCK_MAX_RETRY_INTERVAL = 900 seconds
+            # 900 * 2 = 1800 seconds
+            # 1800 seconds / 100 iterations = 18 seconds per iteration
+            pump_fraction = 2 * WORKER_LOCK_MAX_RETRY_INTERVAL / 100
+            # In case later adjustments to constants causes a drift in the calculation,
+            # let future us know this is not necessarily a fault
+            assert pump_fraction < _LOCK_TIMEOUT_MS, (
+                "Please adjust this test, the calculated pump() iteration exceeds the "
+                f"time the Lock will drop by: {pump_fraction} > {_LOCK_TIMEOUT_MS}"
+            )
+            self.pump(pump_fraction)
+
+            # Should be called 1 or 2 times, there is a jitter to account for
+            call_count = wrapped_try_acquire_lock_method.call_count
+            assert 0 < call_count < 3, (
+                f"Count of times try_to_acquire() was called was out of presumed bounds(> 0 but < 3): {call_count}"
+            )
             self.get_success(lock1.__aexit__(None, None, None))
 
             self.get_success(d2)
@@ -161,26 +202,64 @@ class WorkerLockWorkersTestCase(BaseMultiWorkerStreamTestCase):
             },
         )
         worker_lock_handler = worker.get_worker_locks_handler()
+        worker_data_store = worker.get_datastores().main
 
         lock1 = self.main_worker_lock_handler.acquire_lock("name", "key")
         self.get_success(lock1.__aenter__())
 
         lock2 = worker_lock_handler.acquire_lock("name", "key")
-        # Wrap the WaitingLock object, so we can detect if the timeouts are being hit
+
+        d2 = defer.ensureDeferred(lock2.__aenter__())
+        self.assertNoResult(d2)
+
+        # Wrap the database call to attempt to acquire the lock, so we can detect how
+        # many times it is called on. With lock1 having already been entered, any other
+        # calls to try_acquire_lock() should only be for lock2
         with patch.object(
-            lock2,
-            "_increment_timeout_interval",
-            wraps=lock2._increment_timeout_interval,
-        ) as wrapped_lock2_increment_timeout_interval_method:
-            d2 = defer.ensureDeferred(lock2.__aenter__())
-            self.assertNoResult(d2)
+            worker_data_store,
+            "try_acquire_lock",
+            wraps=worker_data_store.try_acquire_lock,
+        ) as wrapped_worker_try_acquire_lock_method:
+            # A Lock has an internal background looping call that runs every 30 seconds
+            # to renew the Lock and push it's "drop timeout" value further out by 2
+            # minutes. The Lock will prematurely drop if this renewal is not allowed to
+            # run, which sours the test.
 
-            # Recall that pump() will advance time of the given amount 100 times, this
-            # amounts to about 10 seconds passing
-            self.pump(0.1)
+            # Advance time by a bit over 3 hours. _LOCK_TIMEOUT_MS is 2 minutes. Remove
+            # 1 second from that to give it the barest minimum time to still renew
+            # itself(the test fails if we do not remove that second and the Lock will
+            # drop)
+            # 2 * 60 = 120 seconds
+            # 120 - 1 = 119 seconds to actually advance
+            pump_fraction = (_LOCK_TIMEOUT_MS / 1000) - 1
+            # 119 * 100 = 11_900 seconds for the entire pump call
+            # 11_900 / 60 = 180 minutes and 18.33 seconds
+            self.pump(pump_fraction)  # iterates 100 times, see the math above
+            # The actual Lock should not exist still
+            assert lock2._inner_lock is None
 
-            # Should be timed out 6 times, but do not fail on that exact count
-            wrapped_lock2_increment_timeout_interval_method.assert_called()
+            wrapped_worker_try_acquire_lock_method.reset_mock()
+
+            # By this point, the timeout on the WaitingLock should be maxed out at
+            # WORKER_LOCK_MAX_RETRY_INTERVAL. Wait twice that long using pump() so lock1
+            # doesn't drop in the background causing an incorrect pass for the test.
+            # WORKER_LOCK_MAX_RETRY_INTERVAL = 900 seconds
+            # 900 * 2 = 1800 seconds
+            # 1800 seconds / 100 iterations = 18 seconds per iteration
+            pump_fraction = 2 * WORKER_LOCK_MAX_RETRY_INTERVAL / 100
+            # In case later adjustments to constants causes a drift in the calculation,
+            # let future us know this is not necessarily a fault
+            assert pump_fraction < _LOCK_TIMEOUT_MS, (
+                "Please adjust this test, the calculated pump() iteration exceeds the "
+                f"time the Lock will drop by: {pump_fraction} > {_LOCK_TIMEOUT_MS}"
+            )
+            self.pump(pump_fraction)
+
+            # Should be called 1 or 2 times, there is a jitter to account for
+            call_count = wrapped_worker_try_acquire_lock_method.call_count
+            assert 0 < call_count < 3, (
+                f"Count of times try_to_acquire() was called was out of presumed bounds(> 0 but < 3): {call_count}"
+            )
             self.get_success(lock1.__aexit__(None, None, None))
 
             self.get_success(d2)
