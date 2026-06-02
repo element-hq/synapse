69526539f5bd1a87793ee7e33fd95e54a48d38b9
Jason Little <j.little@famedly.com>
2026-06-02 12:41:20 -0500

    switch from autocommit attribute to property setter to work around __setattr__() missing

--- synapse/storage/engines/psycopg.py
+++ synapse/storage/engines/psycopg.py
@@ -20,7 +20,6 @@ import psycopg.errors
 import psycopg.sql
 
 from twisted.enterprise.adbapi import Connection as TxConnection
-
 from synapse.storage.engines import PostgresEngine
 from synapse.storage.engines._base import IsolationLevel
 
@@ -89,11 +88,7 @@ class PsycopgEngine(
     def attempt_to_set_autocommit(
         self, conn: psycopg.Connection, autocommit: bool
     ) -> None:
-        # Sometimes this gets called with a Twisted connection instead, unwrap
-        # it because it doesn't support __setattr__.
-        if isinstance(conn, TxConnection):
-            conn = conn._connection
-        conn.autocommit = autocommit
+        conn.set_autocommit(autocommit)
 
     def attempt_to_set_isolation_level(
         self, conn: psycopg.Connection, isolation_level: IsolationLevel | None = None
