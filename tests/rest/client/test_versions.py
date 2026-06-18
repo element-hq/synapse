8270087c3b6c2fa989e215d754a74dca0874c36d
Eric Eastwood <erice@element.io>
2026-06-17 19:55:20 -0500

    Fix test failing with `RuntimeError: Tokio runtime is not running`
    `_trial_temp/test.log`
    ```
    2026-06-17 19:17:57-0500 [-] synapse.http.server - 147 - ERROR - GET-3 - Failed handle request via 'VersionsRestServlet': <SynapseRequest at 0x7ff02b1cf9d0 method='GET' uri='/_matrix/client/versions' clientproto='1.1' site='test'>
    	Traceback (most recent call last):
    	  File "/home/eric/Documents/github/element/synapse/synapse/http/server.py", line 335, in _async_render_wrapper
    	    callback_return = await self._async_render(request)
    	                      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    	  File "/home/eric/Documents/github/element/synapse/synapse/http/server.py", line 576, in _async_render
    	    callback_return = await raw_callback_return
    	                      ^^^^^^^^^^^^^^^^^^^^^^^^^
    	  File "/home/eric/Documents/github/element/synapse/synapse/rest/client/versions.py", line 81, in on_GET
    	    versions_response_body = await self.rust_handlers.versions.get_versions(user_id)
    	                                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^^^^^^^^^
    	RuntimeError: Tokio runtime is not running
    ```

--- tests/rest/client/test_versions.py
+++ tests/rest/client/test_versions.py
@@ -19,6 +19,7 @@ from synapse.rest.client import login, versions
 from synapse.server import HomeServer
 from synapse.util.clock import Clock
 from synapse.types import JsonDict
+from synapse.synapse_rust.http_client import HttpClient
 
 from tests import unittest
 
@@ -35,6 +36,34 @@ class VersionsTestCase(unittest.HomeserverTestCase):
         login.register_servlets,
         versions.register_servlets,
     ]
+
+    def make_homeserver(self, reactor: MemoryReactor, clock: Clock) -> HomeServer:
+        hs = self.setup_test_homeserver()
+
+        # XXX: We must create the Rust HTTP client before we call `reactor.run()` below.
+        # Twisted's `MemoryReactor` doesn't invoke `callWhenRunning` callbacks if it's
+        # already running and we rely on that to start the Tokio thread pool in Rust. In
+        # the future, this may not matter, see https://github.com/twisted/twisted/pull/12514
+        self._http_client = hs.get_proxied_http_client()
+        _ = HttpClient(
+            reactor=hs.get_reactor(),
+            user_agent=self._http_client.user_agent.decode("utf8"),
+        )
+
+        # This triggers the server startup hooks, which starts the Tokio thread pool
+        reactor.run()
+
+        return hs
+
+    def tearDown(self) -> None:
+        # MemoryReactor doesn't trigger the shutdown phases, and we want the
+        # Tokio thread pool to be stopped
+        # XXX: This logic should probably get moved somewhere else
+        shutdown_triggers = self.reactor.triggers.get("shutdown", {})
+        for phase in ["before", "during", "after"]:
+            triggers = shutdown_triggers.get(phase, [])
+            for callbable, args, kwargs in triggers:
+                callbable(*args, **kwargs)
 
     def prepare(self, reactor: MemoryReactor, clock: Clock, hs: HomeServer) -> None:
         self.admin_user = self.register_user("admin", "pass", admin=True)
