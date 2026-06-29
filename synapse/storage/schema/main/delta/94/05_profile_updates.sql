1fc8d4c6cd352ce26979afe5d631ae620f659591
Jason Robinson <jasonr@element.io>
2026-06-29 12:39:46 +0300

    Move profile updates SQL to latest delta
    We shouldn't need a delta bump for this feature as far as I can tell.

--- synapse/storage/schema/main/delta/94/05_profile_updates.sql
+++ synapse/storage/schema/main/delta/94/05_profile_updates.sql
@@ -12,7 +12,7 @@
 -- <https://www.gnu.org/licenses/agpl-3.0.html>.
 
 -- Track updates to profile fields for MSC4429 legacy /sync.
-CREATE TABLE profile_updates (
+CREATE TABLE IF NOT EXISTS profile_updates (
   stream_id BIGINT NOT NULL PRIMARY KEY,
   instance_name TEXT NOT NULL,
 
@@ -32,12 +32,12 @@ CREATE TABLE profile_updates (
   inserted_ts BIGINT NOT NULL
 );
 
-CREATE INDEX profile_updates_by_user ON profile_updates (user_id, stream_id);
-CREATE INDEX profile_updates_by_field ON profile_updates (field_name, stream_id);
-CREATE INDEX profile_updates_inserted_ts ON profile_updates (inserted_ts);
+CREATE INDEX IF NOT EXISTS profile_updates_by_user ON profile_updates (user_id, stream_id);
+CREATE INDEX IF NOT EXISTS profile_updates_by_field ON profile_updates (field_name, stream_id);
+CREATE INDEX IF NOT EXISTS profile_updates_inserted_ts ON profile_updates (inserted_ts);
 
 -- Track which local users should receive each profile update.
-CREATE TABLE profile_updates_per_user (
+CREATE TABLE IF NOT EXISTS profile_updates_per_user (
   stream_id BIGINT NOT NULL REFERENCES profile_updates (stream_id),
 
   -- The full user ID of the local user that should receive the profile update.
@@ -48,5 +48,5 @@ CREATE TABLE profile_updates_per_user (
   inserted_ts BIGINT NOT NULL
 );
 
-CREATE INDEX profile_updates_per_user_by_user_stream ON profile_updates_per_user (user_id, stream_id);
-CREATE INDEX profile_updates_per_user_inserted_ts ON profile_updates_per_user (inserted_ts);
+CREATE INDEX IF NOT EXISTS profile_updates_per_user_by_user_stream ON profile_updates_per_user (user_id, stream_id);
+CREATE INDEX IF NOT EXISTS profile_updates_per_user_inserted_ts ON profile_updates_per_user (inserted_ts);
