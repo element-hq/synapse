cd0de69bc5c57189c177b727cc4809b32abc46e8
reivilibre <reivilibre@users.noreply.github.com>
2026-07-07 14:03:42 +0000

    deploy: ec6c53e7df9d07b0dfd531247b544761b3f940eb

--- develop/searcher-c2a407aa.js
+++ develop/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-dac38eb4.js',
+                path_to_root + 'searchindex-e1cf3bac.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
