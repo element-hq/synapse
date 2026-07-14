1f090f4183a2b9f8265c6145d732dc8282566248
Jason Robinson <jasonr@element.io>
2026-07-14 22:43:41 +0300

    Move persisting profile update actions for membership events into the same transaction as events
    Before persisting the membership event to the database, collect some information for the store so that it can insert the right profile update stream rows regarding membership join and leave events.
    
    This increases transaction safety as we write the profile update rows and the events themselves in the same transaction.

--- synapse/replication/http/profile.py
+++ synapse/replication/http/profile.py
@@ -18,7 +18,6 @@ from typing import TYPE_CHECKING
 
 from twisted.web.server import Request
 
-from synapse.api.constants import Membership
 from synapse.http.server import HttpServer
 from synapse.replication.http._base import ReplicationEndpoint
 from synapse.synapse_rust.types import Requester
@@ -28,63 +27,6 @@ if TYPE_CHECKING:
     from synapse.server import HomeServer
 
 logger = logging.getLogger(__name__)
-
-
-class ReplicationProfileUserRoomMembershipChange(ReplicationEndpoint):
-    """Store user profile update action regarding membership changes.
-
-    The POST looks like:
-
-        POST /_synapse/replication/profile_user_room_membership_change/<user_id>
-
-        {
-            "room_id": "!1234:domain.tld",
-            "membership": "join | leave"
-        }
-
-        200 OK
-
-        {}
-    """
-
-    NAME = "profile_user_room_membership_change"
-    PATH_ARGS = ("user_id",)
-    METHOD = "POST"
-    CACHE = False
-
-    def __init__(self, hs: "HomeServer"):
-        super().__init__(hs)
-
-        self._profile_handler = hs.get_profile_handler()
-
-    @staticmethod
-    async def _serialize_payload(  # type: ignore[override]
-        user_id: str,
-        room_id: str,
-        membership: str,
-    ) -> JsonDict:
-        assert membership in (Membership.JOIN, Membership.LEAVE)
-        return {
-            "room_id": room_id,
-            "membership": membership,
-        }
-
-    async def _handle_request(  # type: ignore[override]
-        self, request: Request, content: JsonDict, user_id: str
-    ) -> tuple[int, JsonDict]:
-        assert content["membership"] in (Membership.JOIN, Membership.LEAVE)
-        if content["membership"] == Membership.JOIN:
-            await self._profile_handler.user_joined_room(
-                user_id=UserID.from_string(user_id),
-                room_id=content["room_id"],
-            )
-        else:
-            await self._profile_handler.user_left_room(
-                user_id=UserID.from_string(user_id),
-                room_id=content["room_id"],
-            )
-
-        return (200, {})
 
 
 class ReplicationProfileSetField(ReplicationEndpoint):
@@ -260,7 +202,6 @@ class ReplicationProfileDeleteUponDeactivation(ReplicationEndpoint):
 
 
 def register_servlets(hs: "HomeServer", http_server: HttpServer) -> None:
-    ReplicationProfileUserRoomMembershipChange(hs).register(http_server)
     ReplicationProfileSetField(hs).register(http_server)
     ReplicationProfileDeleteField(hs).register(http_server)
     ReplicationProfileDeleteUponDeactivation(hs).register(http_server)
