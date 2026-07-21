4a34ff4f9a75b8725dad9d2615775ed027403c97
MadLittleMods <MadLittleMods@users.noreply.github.com>
2026-07-21 15:55:02 +0000

    deploy: 3622579e7bc9fe70d3682bc20bb4955fbb0c9e82

--- develop/searcher-c2a407aa.js
+++ develop/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f1321359.js',
+                path_to_root + 'searchindex-c124cf9e.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
