1ce1ec98a5ccb932dd81490ff8893eba7b8a0e53
Eric Eastwood <erice@element.io>
2026-07-28 22:59:37 -0500

    Only publish `PREPPED_SQLITE_DB_CONN` once it's fully prepared

--- tests/server.py
+++ tests/server.py
@@ -1248,7 +1248,7 @@ def setup_test_homeserver(
         global PREPPED_SQLITE_DB_CONN
         if PREPPED_SQLITE_DB_CONN is None:
             temp_engine = create_engine(database_config)
-            PREPPED_SQLITE_DB_CONN = LoggingDatabaseConnection(
+            prepped_conn = LoggingDatabaseConnection(
                 conn=sqlite3.connect(":memory:"),
                 engine=temp_engine,
                 default_txn_name="PREPPED_CONN",
@@ -1258,7 +1258,7 @@ def setup_test_homeserver(
             database = DatabaseConnectionConfig("master", database_config)
             config.database.databases = [database]
             prepare_database(
-                PREPPED_SQLITE_DB_CONN,
+                prepped_conn,
                 create_engine(database_config),
                 # We pass `config=None` here so that the template database is prepared the
                 # same way regardless of which test happens to be the first one to run.
@@ -1272,6 +1272,12 @@ def setup_test_homeserver(
                 # module schemas) is still applied per-test.
                 config=None,
             )
+
+            # Only publish the template once it's fully prepared. Previously, this was
+            # assigned before `prepare_database(...)` ran which meant that if
+            # `prepare_database(...)` failed, we ended up with an unitialized/partial
+            # database state and never tried to re-create it for subsequent tests.
+            PREPPED_SQLITE_DB_CONN = prepped_conn
 
         database_config["_TEST_PREPPED_CONN"] = PREPPED_SQLITE_DB_CONN
 
