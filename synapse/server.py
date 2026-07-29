19537992f1089cca46986092da3b809e09aa4a43
Eric Eastwood <erice@element.io>
2026-07-28 21:42:06 -0500

    `start_doing_background_updates` once and for all databases
    The code has evolved since I first put this PR up, see https://github.com/element-hq/synapse/pull/19181

--- synapse/server.py
+++ synapse/server.py
@@ -670,7 +670,8 @@ class HomeServer(metaclass=abc.ABCMeta):
         self.get_task_scheduler()
         self.get_common_usage_metrics_manager().setup()
         start_phone_stats_home(self)
-        self.get_datastores().main.db_pool.updates.start_doing_background_updates()
+        for db in self.get_datastores().databases:
+            db.updates.start_doing_background_updates()
 
     def get_reactor(self) -> ISynapseReactor:
         """
