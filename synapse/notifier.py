6f2b4d3b53ee08c3c927dc86b5a0d25f2193b0f1
Erik Johnston <erik@matrix.org>
2026-08-13 15:01:41 +0100

    Update logging wording

--- synapse/notifier.py
+++ synapse/notifier.py
@@ -922,7 +922,7 @@ class Notifier:
                     ).inc()
 
                 logger.warning(
-                    "Timed out waiting for current token to catch up on %s",
+                    "Timed out this worker's streams to advance to given token: %s",
                     _describe_lagging_streams(
                         stream_token, current_token, lagging_stream_keys
                     ),
@@ -931,7 +931,7 @@ class Notifier:
 
             if not logged:
                 logger.info(
-                    "Waiting for current token to catch up on %s",
+                    "Waiting for this worker's streams to advance to given token: %s",
                     _describe_lagging_streams(
                         stream_token, current_token, lagging_stream_keys
                     ),
