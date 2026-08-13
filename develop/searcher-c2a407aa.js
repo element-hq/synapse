02de1c5a74e0336f43d4177b015a485585352868
erikjohnston <erikjohnston@users.noreply.github.com>
2026-08-13 13:04:46 +0000

    deploy: 0c6714d0d291b1cc2d450b11964038bca424814a

--- develop/searcher-c2a407aa.js
+++ develop/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-7c305fa1.js',
+                path_to_root + 'searchindex-be642c07.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
