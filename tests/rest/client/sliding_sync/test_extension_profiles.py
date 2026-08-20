3b7ef7bdef9fa3bd0266e9d60d7675c3cb72beac
Jason Robinson <jasonr@element.io>
2026-08-20 13:50:53 +0300

    Revert "Add per connection state tracking for profile updates for sliding sync"
    This reverts commit 9250caf5d8377f114fc348c342841e65005b1d14.

--- tests/rest/client/sliding_sync/test_extension_profiles.py
+++ tests/rest/client/sliding_sync/test_extension_profiles.py
@@ -616,51 +616,6 @@ class SlidingSyncProfilesTestCase(SlidingSyncBase):
         deliver them unnecessarily, or can later figure out if we need to send
         fields for new rooms or field request subset changes.
         """
-        new_room = self.helper.create_room_as(self.user, tok=self.tok)
-        # Make an initial Sliding Sync request with the profiles extension enabled
-        profiles_config: dict = {
-            "enabled": True,
-        }
-        sync_body = {
-            "lists": {},
-            "room_subscriptions": {
-                self.joined_room: {
-                    "required_state": [],
-                    "timeline_limit": 10,
-                },
-            },
-            "extensions": {
-                "org.matrix.msc4262.profiles": profiles_config,
-            },
-        }
-        response_body, from_token = self.do_sync(sync_body, tok=self.tok)
-
-        # Starting situation, we get the full profile
-        self.assertEqual(
-            response_body["extensions"]["org.matrix.msc4262.profiles"]["users"][
-                "@other_user:test"
-            ],
-            {
-                "updated": {
-                    "avatar_url": None,
-                    "displayname": "other_user",
-                },
-            },
-        )
-
-        # Join other user to the new room and sync now asking for both
-        self.helper.join(new_room, self.other_user, tok=self.other_tok)
-        sync_body["room_subscriptions"][new_room] = {
-            "required_state": [],
-            "timeline_limit": 10,
-        }
-
-        # Make an incremental Sliding Sync request
-        response_body, _ = self.do_sync(sync_body, since=from_token, tok=self.tok)
-        # We should not get other user re-sent
-        self.assertIsNone(
-            response_body["extensions"].get("org.matrix.msc4262.profiles"),
-        )
 
         # Set a new field value and ensure our tracking doesn't swallow updates
         self.get_success(
