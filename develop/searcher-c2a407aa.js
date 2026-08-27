5a187cf3784a6cc3ac64e0d337aca8ae2a9d90a2
anoadragon453 <anoadragon453@users.noreply.github.com>
2026-08-27 11:24:18 +0000

    deploy: c246d81dec17bbb0d52e4d1b284e80b57b632d69

--- develop/searcher-c2a407aa.js
+++ develop/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-aa48831a.js',
+                path_to_root + 'searchindex-276d8b54.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
