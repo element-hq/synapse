53e01dd9ff459ad28020084990af6c6fd9c783fc
benbz <benbz@users.noreply.github.com>
2026-09-10 12:53:13 +0000

    deploy: c69d7eeb592dec24816a96365508eaeb16204c0b

--- develop/searcher-c2a407aa.js
+++ develop/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-bb6a4dbb.js',
+                path_to_root + 'searchindex-0e749d1b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
