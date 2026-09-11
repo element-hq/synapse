3f17bd706ae9043e31db3e9d24a259ca42c9a312
Paul Chobert <paul@chobert.fr>
2026-09-11 17:54:31 +0200

    Return `200 {}` again for an unset `displayname`/`avatar_url` (#20200)
    Part of: #19414
    
    When
    [MSC4133](https://github.com/matrix-org/matrix-spec-proposals/pull/4133)
    (custom profile fields) was implemented, the returned value for an unset
    display name changed from `200 {}` to `200 { displayname: null }`. This
    happened first on the unstable `uk.tcpip.msc4133` path in #17488
    (1.123.0), then on the stable path when #18635 (1.135.0) unified the
    `displayname`, `avatar_url` and custom field servlets. Neither PR
    discussed the change in review, so it looks like an unintended side
    effect of the refactor rather than a deliberate decision.
    
    The v1.16 spec mandated to change from returning `200 {}` to `404` but
    change was not identified as breaking and was eventually not implemented
    in other clients and server. This PR has a sister MSC that proposes to
    return to the pre-1.16 error codes:
    [MSC4537](https://github.com/matrix-org/matrix-spec-proposals/pull/4537).
    
    Before:
    
    ```
    GET /_matrix/client/v3/profile/@alice:test/displayname
      200 {"displayname": null}
    ```
    
    After:
    
    ```
    GET /_matrix/client/v3/profile/@alice:test/displayname
      200 {}
    ```
    
    ### Pull Request Checklist
    
    <!-- Please read
    https://element-hq.github.io/synapse/latest/development/contributing_guide.html
    before submitting your pull request -->
    
    * [x] Pull request is based on the develop branch
    * [x] Pull request includes a [changelog
    file](https://element-hq.github.io/synapse/latest/development/contributing_guide.html#changelog).
    The entry should:
    - Be a short description of your change which makes sense to users.
    "Fixed a bug that prevented receiving messages from other servers."
    instead of "Moved X method from `EventStore` to `EventWorkerStore`.".
      - Use markdown where necessary, mostly for `code blocks`.
      - End with either a period (.) or an exclamation mark (!).
      - Start with a capital letter.
    - Feel free to credit yourself, by adding a sentence "Contributed by
    @github_username." or "Contributed by [Your Name]." to the end of the
    entry.
    * [x] [Code
    style](https://element-hq.github.io/synapse/latest/code_style.html) is
    correct (run the
    [linters](https://element-hq.github.io/synapse/latest/development/contributing_guide.html#run-the-linters))

--- synapse/rest/client/profile.py
+++ synapse/rest/client/profile.py
@@ -36,7 +36,7 @@ from synapse.http.servlet import (
 )
 from synapse.http.site import SynapseRequest
 from synapse.rest.client._base import client_patterns
-from synapse.types import JsonDict, JsonValue, UserID
+from synapse.types import JsonDict, UserID
 from synapse.util.stringutils import is_namedspaced_grammar
 
 if TYPE_CHECKING:
@@ -145,16 +145,27 @@ class ProfileFieldRestServlet(RestServlet):
         user = UserID.from_string(user_id)
         await self.profile_handler.check_profile_query_allowed(user, requester_user)
 
+        ret: JsonDict = {}
         if field_name == ProfileFields.DISPLAYNAME:
-            field_value: (
-                JsonValue | dict[str, JsonValue]
-            ) = await self.profile_handler.get_displayname(user)
+            displayname = await self.profile_handler.get_displayname(user)
+            if displayname is not None:
+                ret[field_name] = displayname
         elif field_name == ProfileFields.AVATAR_URL:
-            field_value = await self.profile_handler.get_avatar_url(user)
+            avatar_url = await self.profile_handler.get_avatar_url(user)
+            if avatar_url is not None:
+                ret[field_name] = avatar_url
         else:
-            field_value = await self.profile_handler.get_profile_field(user, field_name)
+            # Custom fields deliberately behave differently from `displayname` and
+            # `avatar_url`: an unset custom field raises a 404 rather than returning
+            # `200 {}`. The spec allows both, see MSC4537:
+            # https://github.com/matrix-org/matrix-spec-proposals/pull/4537
+            # This is likely to change once the spec settles on either 404 or
+            # `200 {}` only, which would be a breaking change.
+            ret[field_name] = await self.profile_handler.get_profile_field(
+                user, field_name
+            )
 
-        return 200, {field_name: field_value}
+        return 200, ret
 
     async def on_PUT(
         self, request: SynapseRequest, user_id: str, field_name: str
