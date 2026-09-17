310c3cc253bd5c7e57e18c2a7df5006df559023e
devonh <devonh@users.noreply.github.com>
2026-09-17 18:35:14 +0000

    deploy: 7530874a1250d6ad975b39582a784c594d29a505

--- develop/searcher-c2a407aa.js
+++ develop/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-185d90e6.js',
+                path_to_root + 'searchindex-8d3ef834.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
