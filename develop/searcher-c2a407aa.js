ce84de2ea8a602965e62e2c23a74ccf8ebeaaa6e
barodeur <barodeur@users.noreply.github.com>
2026-09-23 16:38:05 +0000

    deploy: c0b7224e850506c94b9f1594f14585b1547eb363

--- develop/searcher-c2a407aa.js
+++ develop/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-09af8a35.js',
+                path_to_root + 'searchindex-9b1f7488.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
