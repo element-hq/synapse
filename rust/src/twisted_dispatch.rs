c490679bc4139b080e4fe22a8a9134e372f21ff2
Erik Johnston <erikj@element.io>
2026-09-24 13:15:38 +0100

    Update rust/src/twisted_dispatch.rs
    Co-authored-by: Andrew Morgan <1342360+anoadragon453@users.noreply.github.com>

--- rust/src/twisted_dispatch.rs
+++ rust/src/twisted_dispatch.rs
@@ -25,7 +25,7 @@
 //! The implementation combines a queue of closures with the self-pipe trick,
 //! which is the same implementation as Twisted uses for `callFromThread`.
 //!
-//! The [`TwistedDispatcher`] holds a queue of closures and the write end of a
+//! The [`TwistedDispatcher`] worker holds a queue of closures and the write end of a
 //! unix socket pair. A worker pushes a closure and writes one byte. The
 //! [`TwistedDispatchReader`] holds the read end and is registered with the
 //! Twisted reactor via `addReader`. The Twisted reactor is woken up and calls
