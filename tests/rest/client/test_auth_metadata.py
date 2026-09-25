3111d849248a44f5385bee5cecef4b7c216768f7
Richard van der Hoff <richard@matrix.org>
2026-09-25 23:06:13 +0100

    Add test for auth_metadata with MAS enabled

--- tests/rest/client/test_auth_metadata.py
+++ tests/rest/client/test_auth_metadata.py
@@ -19,13 +19,20 @@
 #
 from http import HTTPStatus
 from typing import ClassVar
+from unittest.mock import Mock
 
+from twisted.internet.testing import MemoryReactor
+
+from synapse.api.auth.mas import MasDelegatedAuth
 from synapse.rest.client import auth_metadata
+from synapse.server import HomeServer
+from synapse.types import JsonDict
+from synapse.util.clock import Clock
 
 from tests.unittest import HomeserverTestCase
 
 
-class AuthMetadataTestCase(HomeserverTestCase):
+class AuthMetadataMasDisabledTestCase(HomeserverTestCase):
     endpoint: ClassVar[str]
     servlets = [
         auth_metadata.register_servlets,
@@ -35,3 +42,37 @@ class AuthMetadataTestCase(HomeserverTestCase):
         # Make an unauthenticated request for the discovery info.
         channel = self.make_request("GET", "/_matrix/client/v1/auth_metadata")
         self.assertEqual(channel.code, HTTPStatus.NOT_FOUND)
+
+
+class AuthMetadataMasEnabledTestCase(HomeserverTestCase):
+    servlets = [
+        auth_metadata.register_servlets,
+    ]
+
+    def default_config(self) -> JsonDict:
+        config = super().default_config()
+        # Registration must be disabled for the MAS integration to be enabled.
+        config["enable_registration"] = False
+        config["matrix_authentication_service"] = {
+            "enabled": True,
+            "endpoint": "https://auth.example.com/",
+            "secret": "verysecret",
+        }
+        return config
+
+    def make_homeserver(self, reactor: MemoryReactor, clock: Clock) -> HomeServer:
+        # Install a mock `MasDelegatedAuth` implementation.
+        self.auth = Mock(spec=MasDelegatedAuth)
+        return self.setup_test_homeserver(auth=self.auth)
+
+    def test_returns_metadata_from_mas(self) -> None:
+        test_auth_metadata = {
+            "issuer": "https://auth.example.com/",
+        }
+
+        self.auth.auth_metadata.return_value = test_auth_metadata
+
+        channel = self.make_request("GET", "/_matrix/client/v1/auth_metadata")
+
+        self.assertEqual(channel.code, HTTPStatus.OK, channel.result)
+        self.assertEqual(channel.json_body, test_auth_metadata)
